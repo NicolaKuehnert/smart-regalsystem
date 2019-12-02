69e63dfc2d72308ee633adea347884bea4f78f76
NicolaKuehnert <nicola.kuehnert@outlook.de>
2019-12-02 08:40:24 +0100

    Datentransfer ist jetzt möglich

--- Android App/app/src/main/java/com/example/reader/settings.java
+++ Android App/app/src/main/java/com/example/reader/settings.java
@@ -1,16 +1,18 @@
 package com.example.reader;
 
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 
 import android.content.Intent;
-import android.os.Handler;
 import android.view.View;
 
 import android.widget.AdapterView;
@@ -18,19 +20,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public class settings extends AppCompatActivity {
 
-    private BluetoothAdapter BA;
-    private Set<BluetoothDevice>pairedDevices;
     ListView lv;
-    String DeviceName;
-    String deviceHardwareAddress;
     ArrayList LeDevices;
 
+
+    BLEConnection bleConnection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +41,7 @@ public class settings extends AppCompatActivity {
         getSupportFragmentManager()
                 .beginTransaction()
                 .commit();
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+
 
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
@@ -49,43 +49,48 @@ public class settings extends AppCompatActivity {
         }
 
 
-        BA = BluetoothAdapter.getDefaultAdapter();
         lv = findViewById(R.id.listView);
+        bleConnection = new BLEConnection(this);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
 
                 Object o = lv.getItemAtPosition(position);
-                DeviceName = o.toString();
+                bleConnection.DeviceName = o.toString();
 
-                Toast.makeText(getApplicationContext(),"Connecting to: " + DeviceName, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"Connecting to: " + bleConnection.DeviceName, Toast.LENGTH_SHORT).show();
 
                 try{
-                    if (pairedDevices.size() > 0) {
+                    if (bleConnection.scannedDevices.size() > 0) {
                         // There are paired devices. Get the name and address of each paired device.
-                        for (BluetoothDevice device : pairedDevices) {
-                            if(DeviceName == device.getName()){
-                                deviceHardwareAddress = device.getAddress(); // MAC address
+                        for (BluetoothDevice dev : bleConnection.scannedDevices) {
+                            if(bleConnection.DeviceName.compareTo(dev.getName()) == 0){
+                                bleConnection.device = dev;
+                                bleConnection.deviceHardwareAddress = bleConnection.device.getAddress(); // MAC address
+
+
+                                bleConnection.connect();
                             }
+
                         }
                     }
                 } catch (NullPointerException e){
 
                 }
-
-
-
-
             }
         }
         );
 
-        if(BA.isEnabled()){
+        if(bleConnection.BA.isEnabled()){
             Switch BluetoothSwitch = findViewById(R.id.switch1);
             BluetoothSwitch.setChecked(true);
-            list();
         }
+    }
+
+    public void search(View view){
+        bleConnection.search();
+        list();
     }
 
     public void onSwitch(View v){
@@ -98,8 +103,8 @@ public class settings extends AppCompatActivity {
         }
     }
 
-    public void on(){
-        if (!BA.isEnabled()) {
+    private void on(){
+        if (!bleConnection.BA.isEnabled()) {
             Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnOn, 0);
             Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
@@ -107,62 +112,23 @@ public class settings extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
         }
 
-        list();
     }
 
-    public void off(){
-        BA.disable();
+    private void off(){
+        bleConnection.BA.disable();
         Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
     }
 
-    private boolean mScanning;
-    private Handler handler = new Handler();
+    private void list(){
 
-    // Stops scanning after 10 seconds.
-    private static final long SCAN_PERIOD = 10000;
-
-    // Device scan callback.
-    private BluetoothAdapter.LeScanCallback leScanCallback =
-            new BluetoothAdapter.LeScanCallback() {
-                @Override
-                public void onLeScan(final BluetoothDevice device, int rssi,
-                                     byte[] scanRecord) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            LeDevices.add(device);
-                        }
-                    });
-                }
-            };
-
-    private void scanLeDevice(final boolean enable) {
-
-        if (enable) {
-            // Stops scanning after a pre-defined scan period.
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mScanning = false;
-                    BA.stopLeScan(leScanCallback);
-                }
-            }, SCAN_PERIOD);
-
-            mScanning = true;
-            BA.startLeScan(leScanCallback);
-        } else {
-            mScanning = false;
-            BA.stopLeScan(leScanCallback);
-        }
-    }
-
-    public void list(){
-
-            scanLeDevice(true);
-
-            if(LeDevices == null){
+            LeDevices = new ArrayList<String>();
+            if(bleConnection.scannedDevices == null) {
                 Toast.makeText(getApplicationContext(), "No BLE Devices found!", Toast.LENGTH_LONG).show();
                 return;
+            }
+
+            for(BluetoothDevice device : bleConnection.scannedDevices){
+                LeDevices.add(device.getName());
             }
             final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, LeDevices);
 
@@ -170,4 +136,10 @@ public class settings extends AppCompatActivity {
 
 
     }
+
+    public void disconnect(View v){
+        bleConnection.disconnect();
+
+    }
+
 }
