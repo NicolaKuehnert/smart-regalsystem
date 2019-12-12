e4227bdc2848760319b7a6add992c8ede18ade20
Waldemar Reger <waldemar.reger@fh-bielefeld.de>
2019-12-12 11:51:12 +0100

    Update main.cpp
    Rfid code hinzugefügt

--- Lagersystem/src/main.cpp
+++ Lagersystem/src/main.cpp
@@ -7,10 +7,10 @@
 State state = scan;
 uint32_t targetTime = 0;
 int i_init = 0;
-
-
+// 0x28 is i2c address on SDA. Check your address with i2cscanner if not match.
+MFRC522 mfrc522(0x28);   // Create MFRC522 instance.
 #define sleeptime 1
-
+char *tokensFile = "/datenbank.txt";
 /*
 *Gibt das Menüfeld der Searchfunktion aus.
 *Diese Methode wird ausgeführt wen der "A-Knopf" gedrückt wird und der Zustand >nicht< auf "search" ist.
@@ -84,10 +84,102 @@ int check(ble context){
     return 0;
 }
 
+/***********************************************
+        FUNKTIONEN FÜR DIE SD KARTE
+***********************************************/
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+
+    // Print blank line on screen
+    //M5.Lcd.printf(" \n  ");
+    //M5.Lcd.printf(" \n  ");
+    
+    Serial.printf("Listing directory: %s\n", dirname);
+    //M5.Lcd.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        //M5.Lcd.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        //M5.Lcd.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            //M5.Lcd.print("  DIR : ");
+            Serial.println(file.name());
+            //M5.Lcd.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            //M5.Lcd.print("  FILE: ");
+            Serial.print(file.name());
+            //M5.Lcd.print(file.name());
+            Serial.print("  SIZE: ");
+            //M5.Lcd.print("  SIZE: ");
+            Serial.println(file.size());
+            //M5.Lcd.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void readFile(fs::FS &fs, const char * path) {
+    Serial.printf("Reading file: %s\n", path);
+    //M5.Lcd.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        //M5.Lcd.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    //M5.Lcd.print("Read from file: ");
+    while(file.available()){
+        int ch = file.read();
+        Serial.write(ch);
+        //M5.Lcd.write(ch);
+    }
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+    //M5.Lcd.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        //M5.Lcd.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+        //M5.Lcd.println("File written");
+    } else {
+        Serial.println("Write failed");
+        //M5.Lcd.println("Write failed");
+    }
+}
+
 void setup() {
-  M5.begin(true, false, true);
+  M5.begin(true, true, true);
  
   M5.Power.begin();
+  
+  Wire.begin();
+  writeFile(SD, tokensFile, " ");
+
+  mfrc522.PCD_Init();             // Init MFRC522
   
   printScan();
 }
@@ -119,8 +211,25 @@ void loop() {
     
   } else if (M5.BtnC.wasPressed() && state != scan)
   {
-    state = scan;
-    printScan();
+    //TODO rfid scan funktion
+    if (SD.exists(tokensFile) == true)
+    {
+      if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+        //delay(50);
+        return;
+      }
+
+        M5.Lcd.fillRect(0 ,140, 320, 50 ,0);
+        M5.Lcd.setCursor(70,150);
+      for (byte i = 0; i < mfrc522.uid.size; i++) {
+        M5.Lcd.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        M5.Lcd.print(mfrc522.uid.uidByte[i], HEX);
+      }
+      
+    } else 
+    {
+     Serial.println("Datei nicht gefunden.");
+    }
   }
 }
 
