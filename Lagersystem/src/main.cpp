f01c6e1ca2188a2e22a1f075af08aa50d7f732c5
Waldemar 'Dackel' Reger <wreger3@freenet.de>
2020-01-02 14:31:22 +0100

    Scanfunktion + Suchfunktion
    Jetzt geht das Scanen. die Regalreihen werden statisch angegeben und neuen Tags zugeordnet. Die tags werden im Json format gespeichert, aber in einer txt datei gespeichert ( in den Arduinojson example wird auch nur eine txt verwendet). in der Suchfunktion wird nachgeschaut ob ein Tag schon vorhanden ist und falls ja eine Led angemacht

--- Lagersystem/src/main.cpp
+++ Lagersystem/src/main.cpp
@@ -1,29 +1,32 @@
 #include <Arduino.h>
 #include <M5Stack.h>
 #include <SPI.h>
-#include <MFRC522.h>
+#include <Wire.h>
+#include <SD.h>
+#include <ArduinoJson.h>
 
 #include "printDisplay.h"
 #include "search.h"
 #include "ble.h"
-//#include "MFRC522_I2C.h"
-#include "ArduinoJson.h"
-
+#include "MFRC522_I2C.h"
 #include "led.h"
+
+
 
 
 State state = scan;
 uint32_t targetTime = 0;
 int i_init = 0;
 // 0x28 is i2c address on SDA. Check your address with i2cscanner if not match.
-//MFRC522 mfrc522(0x28);   // Create MFRC522 instance.
+MFRC522 mfrc522(0x28);   // Create MFRC522 instance.
 #define sleeptime 1
-const char *tokensFile = "/datenbank.txt";
+const char *filename = "/datenbank.txt";
+String scannedTag= "";
+String rowLast = "";
+String row1 = " 04 6d 95 a2 ec 5a 81";
+String row2 = " 04 67 95 a2 ec 5a 81";
+bool rowScanned = false;
 
-#define RST_PIN         9           // Configurable, see typical pin layout above
-#define SS_PIN          10         // Configurable, see typical pin layout above
-
-MFRC522 mfrc522(SS_PIN, RST_PIN);
 ble BLE;
 /*
 *Gibt das Menüfeld der Searchfunktion aus.
@@ -72,6 +75,7 @@ void printScan(){
   M5.Lcd.println("Search");
   
 }
+
 /*******************************************
  *      FUNKTIONEN FÜR BLUETOOTH
  ******************************************/
@@ -180,7 +184,7 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
 
 
 // Loads the configuration from a file
-void loadConfiguration(const char *filename) {
+bool saveTag(const char *filename, String &row, String book) {
   // Open file for reading
   File file = SD.open(filename);
 
@@ -194,63 +198,97 @@ void loadConfiguration(const char *filename) {
   if (error)
     Serial.println(F("Failed to read file, using default configuration"));
 
+  file.close();
+
+  for (int i = 0; i < doc[row1].size(); i++)
+  {
+    //M5.Lcd.println(doc[row].getMember(i));
+    if (doc[row1].getElement(i) == book)
+    {
+      M5.Lcd.println("gibes schon");
+      return false;
+    }
+  }
+
+  for (int i = 0; i < doc[row2].size(); i++)
+  {
+   
+    //M5.Lcd.println(doc[row].getMember(i));
+    if (doc[row2].getElement(i) == book)
+    {
+      M5.Lcd.println("gibes schon");
+      return false;
+    }
+  }
+  file = SD.open(filename, FILE_WRITE);
+  doc[row].add(book);
+  if (serializeJson(doc, file) == 0) {
+    M5.Lcd.println(F("Failed to write to file"));
+  }
+  file.close();
+  return true;
 }
 
-// Saves the configuration to a file
-void saveConfiguration(const char *filename) {
-  // Delete existing file, otherwise the configuration is appended to the file
-  SD.remove(filename);
+void searchTag(const char *filename, String &book)
+{
 
-  // Open file for writing
-  File file = SD.open(filename, FILE_WRITE);
-  if (!file) {
-    Serial.println(F("Failed to create file"));
-    return;
-  }
+  // Open file for reading
+  File file = SD.open(filename);
 
   // Allocate a temporary JsonDocument
   // Don't forget to change the capacity to match your requirements.
-  // Use arduinojson.org/assistant to compute the capacity.
-  StaticJsonDocument<256> doc;
+  // Use arduinojson.org/v6/assistant to compute the capacity.
+  StaticJsonDocument<512> doc;
 
-  // Set the values in the document
- /* doc["hostname"] = config.hostname;
-  doc["port"] = config.port;*/
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, file);
+  if (error)
+    Serial.println(F("Failed to read file, using default configuration"));
 
-  // Serialize JSON to file
-  if (serializeJson(doc, file) == 0) {
-    Serial.println(F("Failed to write to file"));
+  M5.Lcd.setCursor(0, 150);
+  file.close();
+  if (doc[row1].getMember(book) != NULL)
+  {
+    M5.Lcd.println("reihe 1");
+    //redLed();
   }
+  else if (doc[row2].getMember(book) != NULL)
+  {
+    //yellowLed();
+    M5.Lcd.println("reihe 2");
+  }
+  else
+  {
+    M5.Lcd.println("gibt es nicht");
+  }
+  
+}
+
+// Prints the content of a file to the Serial
+void printFile(const char *filename) {
+  // Open file for reading
+  File file = SD.open(filename);
+  if (!file) {
+    M5.Lcd.println(F("Failed to read file"));
+    return;
+  }
+
+  // Extract each characters by one by one
+  while (file.available()) {
+    M5.Lcd.print((char)file.read());
+  }
+  M5.Lcd.println();
 
   // Close the file
   file.close();
 }
 
-/**
- * Helper routine to dump a byte array as hex values to Serial. 
- */
-void printHex(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    M5.Lcd.print(buffer[i] < 0x10 ? " 0" : " ");
-    M5.Lcd.print(buffer[i], HEX);
-  }
-}
-
-/**
- * Helper routine to dump a byte array as dec values to Serial.
- */
-void printDec(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    M5.Lcd.print(buffer[i] < 0x10 ? " 0" : " ");
-    M5.Lcd.print(buffer[i], DEC);
-  }
-}
-
 /**************************************
  *      FUNKTIONEN FÜR DIE LED
  *************************************/
-void ledtest(){
-  led red(5);
+void redLed()
+{
+led red(5);
   led yellow(2);
 
   try{
@@ -286,10 +324,116 @@ void ledtest(){
     }
 
   }
-
-  
 }
 
+void yellowLed()
+{
+led red(5);
+
+  try{
+    int status = red.init();
+    if(status == 1){
+      throw 101;
+    }
+    red.on();
+    sleep(sleeptime);
+    red.off();
+
+  } catch(int i) {
+
+    switch(i){
+      case 101:
+        M5.Lcd.clearDisplay();
+        M5.Lcd.setCursor(0,0);
+        M5.Lcd.println("Error 101: Red LED not initialised");
+        break;
+    }
+
+  }
+}
+void ledtest(){
+  
+  led yellow(2);
+
+  try{
+    int status = yellow.init();
+    if(status == 1){
+      throw 102;
+    }
+
+    yellow.on();
+    sleep(sleeptime);
+    yellow.off();
+
+  } catch(int i) {
+
+    switch(i){
+      case 102:
+        M5.Lcd.clearDisplay();
+        M5.Lcd.setCursor(0,0);
+        M5.Lcd.println("Error 102: Yellow LED not initialised");
+        break;
+    }
+
+  }
+}
+
+void scanTag()
+{
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+    delay(50);
+    return;
+  }
+
+  M5.Lcd.println(" ");
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    scannedTag += String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    scannedTag += String(mfrc522.uid.uidByte[i], HEX);
+  }
+
+  if ( rowScanned == false)
+  {
+    M5.Lcd.setCursor(0, 150);
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.print("erster Scan: ");
+    M5.Lcd.setCursor(145, 150);
+    if (scannedTag == row1)
+    {
+      M5.Lcd.println("Reihe 1");
+      rowScanned = true;
+      rowLast = scannedTag;
+    } else if (scannedTag == row2)
+    {
+      M5.Lcd.println("Reihe 2");
+      rowScanned = true;
+      rowLast = scannedTag;
+    } else
+    {
+      M5.Lcd.print("Reihe nicht gefunden");
+    }
+  } else if (rowScanned == true)
+  {
+    M5.Lcd.setCursor(0, 170);
+    M5.Lcd.print("zweiter Scan: ");
+    if (scannedTag != row1 && scannedTag != row2) 
+    {
+      if(saveTag(filename, rowLast, scannedTag) == true)
+      {
+        M5.Lcd.setCursor(150, 170);
+        M5.Lcd.print(scannedTag);
+          //printFile(filename);
+        rowScanned = false;
+      }
+    } else 
+    {
+      M5.Lcd.setCursor(150, 170);
+      M5.Lcd.print("Kein Ordner");
+    }
+   }
+    scannedTag.clear();
+    delay(1000);
+}
 /**************************************
  *      MAIN PROGRAMM
  *************************************/
@@ -297,67 +441,49 @@ void setup() {
   M5.begin(true, true, true);
   M5.Power.begin();
   Wire.begin();
-
-  writeFile(SD, tokensFile, "{ \"96 28 57 F7\": true, \"66 20 6C F7\": 3.141 }");
-
- 
-  mfrc522.PCD_Init(); // Init MFRC522 
-
- 
-
-  /*char input[] = "{\"96 28 57 F7\":[48.756080,2.302038],\"66 20 6C F7\":[48.756080,2.302038]}";
-  StaticJsonDocument<250> doc;
-  DeserializationError error = deserializeJson(doc, input);
-
-  // Test if parsing succeeds.
-  if (error) {
-    M5.Lcd.println(F("deserializeJson() failed: "));
-    M5.Lcd.println(error.c_str());
-    return;
-  }
-  const char* sensor = doc["failure"];
-  double latitude = doc["66 20 6C F7"][0];
-  double longitude = doc["66 20 6C F7"][1];*/
-
-  // Print values.
- /* M5.Lcd.println(sensor);
-  M5.Lcd.println(latitude, 6);
-  M5.Lcd.println(longitude, 6);
-  M5.Lcd.println("TESTTTTTT");
+  mfrc522.PCD_Init();
   
-  listDir(SD,"/",0);*/
-  //delay(10000);
+  // Initialize SD library
+  while (!SD.begin()) {
+    Serial.println(F("Failed to initialize SD library"));
+    delay(1000);
+  }
+  SD.remove(filename);
+  if (! SD.exists(filename))
+  {
+    StaticJsonDocument<200> doc;
+    char json[] =
+      "{\" 04 6d 95 a2 ec 5a 81\":[],\" 04 67 95 a2 ec 5a 81\":[]}";
+
+    // Deserialize the JSON document
+    deserializeJson(doc, json);
+    // Open file for writing
+    File file = SD.open(filename, FILE_WRITE);
+    if (serializeJson(doc, file) == 0) {
+      M5.Lcd.println(F("Failed to write to file"));
+    }
+    file.close();
+  }
+ 
+  
+
+ 
+
   
   printScan();
-        M5.Lcd.clear(BLACK);
-      M5.Lcd.setCursor(0,0);
+
 }
 
 void loop() {
   // update button state
-  //M5.update();
-  // Look for new cards, and select one if present
-  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
-    delay(50);
-    return;
-  }
-  
-  // Now a card is selected. The UID and SAK is in mfrc522.uid.
-  
-  // Dump UID
-  Serial.print(F("Card UID:"));
-  M5.Lcd.println(" ");
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-    M5.Lcd.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    M5.Lcd.print(mfrc522.uid.uidByte[i], HEX);
-  } 
-  Serial.println();
-  /*if (M5.BtnA.wasPressed() && state != search)
+  M5.update();
+
+  if (M5.BtnA.wasPressed() && state != search)
   {
     state = search;
     printSearch();
+    String test =  " 04 4f 95 a2 ec 5a 81";
+    searchTag(filename, test);
   } else if (M5.BtnB.wasPressed() && state != scan)
   {
     if(i_init == 0){
@@ -381,68 +507,8 @@ void loop() {
     printScan();
   } else if (M5.BtnB.wasPressed() && state == scan)
   {
-    
-    //TODO rfid scan funktion
-    M5.Lcd.clear(BLACK);
-    M5.Lcd.setCursor(0,0);
-    if (SD.exists(tokensFile) == true)
-    {
-      M5.Lcd.print("TEEST3");
-      if ( ! rfid.PICC_IsNewCardPresent() || ! rfid.PICC_ReadCardSerial())
-      {
-        M5.Lcd.print("TEEST4");
-        return;
-      }
-      M5.Lcd.print("TEEST2");
-
-      Serial.print(F("PICC type: "));
-      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-      Serial.println(rfid.PICC_GetTypeName(piccType));
-
-      // Check is the PICC of Classic MIFARE type
-      if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-          piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-          piccType != MFRC522::PICC_TYPE_MIFARE_4K) 
-      {
-            Serial.println(F("Your tag is not of type MIFARE Classic."));
-            return;
-      }
-
-      if (rfid.uid.uidByte[0] != nuidPICC[0] || 
-          rfid.uid.uidByte[1] != nuidPICC[1] || 
-          rfid.uid.uidByte[2] != nuidPICC[2] || 
-          rfid.uid.uidByte[3] != nuidPICC[3] )
-      {
-          Serial.println(F("A new card has been detected."));
-          
-          // Store NUID into nuidPICC array
-          for (byte i = 0; i < 4; i++) {
-          nuidPICC[i] = rfid.uid.uidByte[i];
-          }
-   
-          M5.Lcd.println(F("The NUID tag is:"));
-          M5.Lcd.print(F("In hex: "));
-          printHex(rfid.uid.uidByte, rfid.uid.size);
-          M5.Lcd.println();
-          M5.Lcd.print(F("In dec: "));
-          printDec(rfid.uid.uidByte, rfid.uid.size);
-          M5.Lcd.println();
-      } else Serial.println(F("Card read previously."));
-
-  // Halt PICC
-  rfid.PICC_HaltA();
-
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
-    M5.Lcd.print("TEEST");
-    delay(1000);
-    printScan();
-  } else 
-    {
-     Serial.println("Datei nicht gefunden.");
-    }
-
-  }*/
+    scanTag();
+  }
 }
 
 
